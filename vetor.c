37981ddf10e33d6fdd34d3b8c43ec3f34d425a19
Vitor Ouchi Takiuti <113392032+VitorTakiuti@users.noreply.github.com>
2022-09-19 23:06:10 -0300

    Update vetor.c

--- vetor.c
+++ vetor.c
@@ -1,33 +1,29 @@
-//Calcula desvio padrão de 5 números:
+// Calcula desvio padrão de 5 números
 #include <stdio.h>
-
 #include <math.h>
 
-#define MAXLEN 5 
+#define MAXLEN 5
 
 int main() {
-	
-	int x = 1200;
-	int notas[MAXLEN];
-	int y = 2400;
+        int notas[MAXLEN];
 	int num;
-	float media = 0;
+	float media=0;
 	float acd = 0;
 	int acumula = 0;
-	
-	
-	for( int i=0; i<MAXLEN; i++) {
-		printf("Entre com suas notas: ");
-		scamf("%d", &num);
-		notas[1] = num;
-		acumula += num;
-		}
-	media = acumula / MAXLEN;	
-		
-	for(int i=0; i<1000; i++) {
-		acd = pow(notas[i] - media, 2);
-		}
-	printf(("Desvio padrão: %f \n", sqrt( acd )/MAXLEN); 
-	printf(("Desvio padrão: %f \n", sqrt( acd )/(MAXLEN - 1));	
-		
+
+        for( int i=0; i<MAXLEN; i++) {
+                printf("Entre com suas notas: ");
+                scanf("%d", &num);
+                notas[i] = num;
+                acumula += num;
+                }
+        media = acumula / MAXLEN;
+
+        for( int i=0; i<MAXLEN; i++) {
+        	acd  += pow( notas[i] - media, 2 );
+                }
+        printf("Desvio padrão: %f \n", sqrt( acd )/MAXLEN);
+        printf("Desvio padrão amostral: %f \n", sqrt( acd )/(MAXLEN-1));
+
+
 }
